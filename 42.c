1d4379afdc020d3de101d8595975177a7223cb98
Audrey <aerv@ic.ufal.br>
2020-02-10 16:57:05 +0000

    Eleitor

---
+++ 42.c
@@ -0,0 +1,17 @@
+#include <stdio.h>	
+int main()
+{
+  int x;
+	scanf("%d", &x);
+    if (18<=x && x<=65){
+      printf("eleitor obrigatorio");
+    }
+    else if(16<=x && x<18 || x>=66){
+      printf("eleitor facultativo");
+    }
+    else if (x<16)
+    {
+      printf("nao eleitor");
+    }
+	return 0;
+}
